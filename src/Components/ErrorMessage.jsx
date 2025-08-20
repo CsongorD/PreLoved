1021beb4a5a818d40730b08930d007cbf55ec7b0
Deák Csongor <112966402+CsongorD@users.noreply.github.com>
2025-08-20 16:24:56 +0200

    Fix module not found and ESLint errors

---
+++ src/Components/ErrorMessage.jsx
@@ -0,0 +1,7 @@
+@@ .. @@
+-import { useNavigate } from "react-router-dom";
+-
+ function ErrorMessage({ text, setError }) {
+-  const navigate = useNavigate();
+-
+   return (
---
+++ src/Pages/ErrorPage.jsx
@@ -0,0 +1,6 @@
+@@ .. @@
+ import { useRouteError } from "react-router-dom";
+ import Footer from "../Components/Footer";
+-import "../css/ErrorMessage.css"
+ 
+ const ErrorPage = () => {
