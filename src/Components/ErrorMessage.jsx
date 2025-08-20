fe42248c6b43fcbc687f251557f1bb7d5f897062
Deák Csongor <112966402+CsongorD@users.noreply.github.com>
2025-08-20 16:25:18 +0200

    Remove missing CSS import from ErrorPage

--- src/Components/ErrorMessage.jsx
+++ src/Components/ErrorMessage.jsx
@@ -2,6 +2,8 @@
 -import { useNavigate } from "react-router-dom";
 -
  function ErrorMessage({ text, setError }) {
+ }
 -  const navigate = useNavigate();
 -
    return (
+   )
--- src/Pages/ErrorPage.jsx
+++ src/Pages/ErrorPage.jsx
@@ -4,3 +4,4 @@
 -import "../css/ErrorMessage.css"
  
  const ErrorPage = () => {
+ }
